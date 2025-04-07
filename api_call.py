3bc453ae6886ae2d924f71e55ab0b5280aba41a0
n2ptr <xuzhanchaomail@163.com>
2025-04-07 17:18:50 +0800

    update platform and models

--- api_call.py
+++ api_call.py
@@ -4,13 +4,24 @@ import time
 def generate_with_api(
         messages: list, 
         api_key, 
-        base_url, 
-        path,
-        max_tokens=50
+        base_url = "https://api.deepseek.com", 
+        path = "deepseek-reasoner",
+        args = None
     ):
+    assert args is not None, "args must be provided for API generation, this assertion fails due to legacy code"
+    # print(f"API call with path: {path}")
     from openai import OpenAI
     success = False
     timeout = 600
+    max_tokens = 50
+    if args.cot:
+        max_tokens = 30000
+        timeout = 3000
+    if "DeepSeek-R1" in path or "o3-mini" in path or "deepseek-reasoner" in path or "deepseek-r1" in path: # reasoning models
+        # max_tokens = 2500
+        max_tokens = 30000
+        timeout = 3000
+    response = None
     while not success:
         try:
             client = OpenAI(api_key=api_key, base_url=base_url)
@@ -18,18 +29,26 @@ def generate_with_api(
                 model=path,
                 messages=messages,
                 timeout=timeout,
-                max_completion_tokens=max_tokens
+                max_completion_tokens=max_tokens,
+                stream=stream
             )
             success = True
         except Exception as e:
             print(f"API call failed: {e}, retrying...")
     
+    assert response is not None, "Response should not be None"
 
     assert response.choices[0].message.role == "assistant"
     
     content = response.choices[0].message.content
     
+    reasoning_content = None # only for deepseek-reasoner
+    if path == "deepseek-reasoner":
+        reasoning_content = response.choices[0].message.reasoning_content
+        content = f"<think>\n\n{reasoning_content}</think>\n\n{content}"
+        
     usage = response.usage
+    
     
     return content, usage
 
@@ -54,8 +73,6 @@ def generate_with_api_zhipu(
     except Exception as e:
         print(f"API call failed: {e}, retrying...")
         time.sleep(0.5)
-    
-    
 
     assert response.choices[0].message.role == "assistant"
     
@@ -65,6 +82,36 @@ def generate_with_api_zhipu(
     
     return content, usage
 
+
+def generate_with_api_azure(
+        messages: list, 
+        api_key, 
+        base_url, 
+        path
+    ):
+    from openai import AzureOpenAI
+    client = AzureOpenAI( api_key=api_key,api_version='2023-05-15', azure_endpoint=base_url)
+    success = False
+    while not success:
+        try:
+            response = client.chat.completions.create(
+                model=path,
+                messages=messages,
+                timeout=600,
+                max_completion_tokens=50,
+            )
+            success = True
+        except Exception as e:
+            print(f"API call failed: {e}, retrying...")
+            time.sleep(2)
+
+    assert response.choices[0].message.role == "assistant"
+    
+    content = response.choices[0].message.content
+    
+    usage = response.usage
+    
+    return content, usage
 
 if __name__ == "__main__":
     #### Example usage ####
@@ -81,15 +128,23 @@ if __name__ == "__main__":
     response = None
     usage = None
     
-    if "glm" in model:
-        response, usage = generate_with_api_zhipu(
+    if model2api_auth[model]["api_type"] == "openai":
+        response, usage = generate_with_api(
             messages=messages, 
             api_key=model2api_auth[model]["api_key"], 
             path=model2path[model],
             max_tokens=50
         )
-    else:
-        response, usage = generate_with_api(
+    elif model2api_auth[model]["api_type"] == "zhipu":
+        response, usage = generate_with_api_zhipu(
+            messages=messages, 
+            api_key=model2api_auth[model]["api_key"], 
+            base_url=model2api_auth[model]["base_url"], 
+            path=model2path[model],
+            max_tokens=50
+        )
+    elif model2api_auth[model]["api_type"] == "azure":
+        response, usage = generate_with_api_azure(
             messages=messages, 
             api_key=model2api_auth[model]["api_key"], 
             base_url=model2api_auth[model]["base_url"], 
